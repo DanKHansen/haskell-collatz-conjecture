719b9d968f652bc693220dad1f5d483c650d2c6c
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-09 10:06:52 +0200

    solution

--- src/CollatzConjecture.hs
+++ src/CollatzConjecture.hs
@@ -1,4 +1,11 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = error "You need to implement this function."
+collatz n = loop n 0
+
+loop :: Integer -> Integer -> Maybe Integer
+loop n acc
+  | n <= 0 = Nothing
+  | n == 1 = Just acc
+  | even n = loop (div n 2) (acc + 1)
+  | otherwise = loop (3 * n + 1) (acc + 1)
