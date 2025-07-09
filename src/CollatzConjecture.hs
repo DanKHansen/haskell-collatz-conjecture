7de1edc229ee4ca9034d7617f2d1a370f3bd7092
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-09 10:29:53 +0200

    refactor

--- src/CollatzConjecture.hs
+++ src/CollatzConjecture.hs
@@ -3,9 +3,8 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n
-  | n > 0 = Just (loop 0 n)
-  | otherwise = Nothing
+collatz n =
+  if n > 0 then Just (loop n 0) else Nothing
   where
-    loop !acc 1 = acc
-    loop !acc i = loop (acc + 1) (if even i then div i 2 else 3 * i + 1)
+    loop 1 !acc = acc
+    loop i !acc = loop (if even i then div i 2 else 3 * i + 1) (acc + 1)
